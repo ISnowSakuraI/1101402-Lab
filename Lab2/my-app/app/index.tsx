650090d9ee4d21ff15100b57e215c3a890fff0a9
SnowSakura <Diamondgametv@gmail.com>
2025-08-21 09:59:16 +0700

    Fix: Lab2

--- Lab2/my-app/app/index.tsx
+++ Lab2/my-app/app/index.tsx
@@ -8,21 +8,21 @@ const App = () => {
       <SafeAreaView style={styles.container}>
         {/* Top Section */}
         <View style={[styles.topSection, styles.lightBlueBackground]}>
-          <View style={styles.box1} />
-          <View style={styles.box2}>
+          <View style={styles.box} />
+          <View style={styles.box}>
             <Text style={styles.greetingText}>Hi, Suranaree</Text>
             <Text style={styles.instructionText}>
               Choose your favourite shoe
             </Text>
           </View>
-          <View style={styles.box3} />
-          <View style={styles.box4} />
+          <View style={styles.box} />
+          <View style={styles.box} />
         </View>
 
         {/* Bottom Section */}
         <View style={[styles.bottomSection, styles.yellowBackground]}>
-          <View style={styles.box5} />
-          <View style={styles.box6}>
+          <View style={styles.box1} />
+          <View style={styles.box2}>
             <Text style={styles.recommendedText}>Recommended for you</Text>
           </View>
         </View>
@@ -49,42 +49,27 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: "#000",
   },
+  box: {
+    flex: 1.5,
+    borderWidth: 1,
+    borderColor: "#000",
+  },
   box1: {
-    flex: 1.5,
-    borderWidth: 1,
-    borderColor: "#000",
-  },
-  box2: {
-    flex: 1.5,
-    borderWidth: 1,
-    borderColor: "#000",
-  },
-  box3: {
-    flex: 1.5,
-    borderWidth: 1,
-    borderColor: "#000",
-  },
-  box4: {
-    flex: 1.5,
-    borderWidth: 1,
-    borderColor: "#000",
-  },
-  box5: {
     flex: 2.5,
     borderWidth: 1,
     borderColor: "#000",
     backgroundColor: "#FFFF00",
   },
-  box6: {
+  box2: {
     flex: 7.5,
   },
   greetingText: {
     fontSize: 16,
-    fontWeight: "bold",
     color: "#000",
   },
   instructionText: {
     fontSize: 14,
+    fontWeight: "bold",
     color: "#000",
   },
   recommendedText: {
